2399657dbdd469fdde9491a1a381d66a55c33f01
Gento <a551.return@gmail.com>
2023-01-12 00:34:06 +0900

    List<TextEditingController>で複数の入力値を保持

--- lib/features/woman_name/woman_name_screen.dart
+++ lib/features/woman_name/woman_name_screen.dart
@@ -7,16 +7,53 @@ class WomanNameScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var _screenSize = MediaQuery.of(context).size;
     final numberOfMember = ref.watch(numberProvider);
+
+    List<TextEditingController> _nameController =
+        List.generate(numberOfMember!, (i) => TextEditingController());
+
     return Scaffold(
         body: Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           const Text(
-            '参加する女性陣の名前を入力してね〜',
+            '参加する女性陣の名前を入力してな〜',
           ),
-          Text(numberOfMember.toString())
+          Text(numberOfMember.toString()),
+          SizedBox(
+            width: _screenSize.width * 0.8,
+            height: _screenSize.height * 0.4,
+            child: ListView.builder(
+                itemCount: numberOfMember,
+                itemBuilder: (BuildContext context, int index) {
+                  int memberIndex = index + 1;
+                  String memberIndexText = memberIndex.toString();
+                  return TextFormField(
+                    controller: _nameController[index],
+                    maxLines: 1,
+                    textInputAction: TextInputAction.next,
+                    decoration:
+                        InputDecoration(hintText: '$memberIndexText人目の名前'),
+                  );
+                }),
+          ),
+          SizedBox(
+            width: _screenSize.width * 0.7,
+            child: ElevatedButton(
+              onPressed: () {
+                print(_nameController[0].text);
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: const Text('次へ'),
+            ),
+          ),
         ],
       ),
     ));
--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// 参加人数
 final numberProvider = StateProvider<int?>((ref) {
-  // 変化するデータ
   return 5;
+});
+
+// 女性参加者の名前
+final womanNameProvider = StateProvider<List<String>>((ref) {
+  return [];
 });
