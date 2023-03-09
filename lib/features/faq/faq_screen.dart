425294ee1b820482f2314e67d5922bd5132b20c1
Gento <a551.return@gmail.com>
2023-03-10 00:18:27 +0900

    faq一旦OK

--- lib/features/faq/faq_screen.dart
+++ lib/features/faq/faq_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:lovemageddon/theme/pallete.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FaqScreen extends StatelessWidget {
   const FaqScreen({super.key});
@@ -31,7 +33,7 @@ class FaqScreen extends StatelessWidget {
                   padding:
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   child: Text(
-                    '女男で別々の人数なんですけど！！',
+                    '女男で別々の人数なんですけど！！🥺',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
                 ),
@@ -47,10 +49,66 @@ class FaqScreen extends StatelessWidget {
                   ),
                   padding:
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text.rich(TextSpan(children: [
+                    const TextSpan(text: 'もっと自由度が高い'),
+                    TextSpan(
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue,
+                        ),
+                        text: "web版",
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            final uri = Uri.parse("https://0414.works/couple/");
+                            launchUrl(uri);
+                          }),
+                    const TextSpan(text: 'ならできます！'),
+                  ])),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  width: _screenSize.width * 0.6,
+                  decoration: BoxDecoration(
+                    color: Pallete.primaryPaleColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   child: Text(
-                    'もっと自由度が高いweb版ならできます！',
+                    '不具合とか要望とか、色々と言いたいことあるんですけど！！😊',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: _screenSize.width * 0.6,
+                  decoration: BoxDecoration(
+                    color: Pallete.secondaryPaleColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Text.rich(TextSpan(children: [
+                    TextSpan(
+                        style: const TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue,
+                        ),
+                        text: "ここのフォーム",
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            final uri = Uri.parse(
+                                "https://docs.google.com/forms/d/e/1FAIpQLSdVLxLJFA8MWr2pBX92Aq0zujjVVkrSXUiq3TRIaPkJi_IhMQ/viewform");
+                            launchUrl(uri);
+                          }),
+                    const TextSpan(text: 'から送るときっと開発者の元に届くはず！（いずれは）'),
+                  ])),
                 ),
               ),
             ],
