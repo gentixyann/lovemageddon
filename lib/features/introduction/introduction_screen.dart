ee376f6b1976b5dc56c70729f87efca9a800c684
Gento <a551.return@gmail.com>
2023-09-17 23:41:14 +0900

    introduction screenを追加

---
+++ lib/features/introduction/introduction_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Introduction_Screen extends StatefulWidget {
+  const Introduction_Screen({super.key});
+
+  @override
+  State<Introduction_Screen> createState() => _Introduction_ScreenState();
+}
+
+class _Introduction_ScreenState extends State<Introduction_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
